188a9ccba0c16000bc3c2fe43b143df542af8be3
Adam Walsh <adamwalsh14@gmail.com>
2015-04-05 17:16:13 -0400

    Param for init is now username or optional access token both of which will set both the soundcloud client object as well as the current user's id value for future use

--- soundcloud_stream_builder.py
+++ soundcloud_stream_builder.py
@@ -4,11 +4,14 @@ import soundcloud
 
 class soundcloud_stream_builder(object):
 
-    def __init__(self, user_id, access_token = None):
+    def __init__(self, username, access_token=None):
         if access_token is None:
-            self.sc = soundcloud.Client("265aecdf421a49f07a6c7f36a049f1b6")
+            self.sc = soundcloud.Client(client_id="265aecdf421a49f07a6c7f36a049f1b6")
+            url = 'https://soundcloud.com/' + username
+            self.my_id = self.sc.get('/resolve', url=url).id
         else:
             self.sc = soundcloud.Client(access_token=access_token)
+            self.my_id = self.sc.get('/me').id
 
     def get_all_following(self):
         pass
@@ -17,6 +20,6 @@ class soundcloud_stream_builder(object):
         pass
 
 if __name__=="__main__":
-    user_id = int(raw_input("user id: "))
-    stream = soundcloud_stream_builder(user_id)
+    username = raw_input("username: ")
+    stream = soundcloud_stream_builder(username)
 
