f8e18738856c82cdf51fcf5dc2a08076136546ca
Adam Walsh <adamwalsh14@gmail.com>
2015-10-14 00:13:51 -0700

    Adds use of linked_partition for paging :D now will get all yo artists

--- soundcloud_stream_builder.py
+++ soundcloud_stream_builder.py
@@ -15,20 +15,43 @@ class soundcloud_stream_builder(object):
             self.sc = soundcloud.Client(access_token=access_token)
             self.my_id = self.sc.get('/me').id
         recent = list()
+        print("gathering the users you follow...")
         for artist in self.get_all_following():
-            print "Getting tracks for artist - " + artist.username
-            recent.extend(self.get_most_recent_tracks(artist.id))
+            print "Getting tracks for artist - " + artist['username']
+            recent.extend(self.get_most_recent_tracks(artist['id']))
         recent.sort(key=lambda track: self.sc_timestamp_to_datetime(track.created_at))
         for track in recent:
-            print track.title + ' ' + track.url
+            print track.title
 
-    def get_all_following(self):
-        return self.sc.get('/users/' + str(self.my_id) + '/followings')
+    def get_all_following(self, offset=0, limit=100):
+        resp = self.sc.get('/users/' + str(self.my_id) + '/followings',
+                linked_partitioning=1, limit=limit, offset=offset * limit)
+        result = []
+        try:
+            next_href = resp.next_href
+            print(next_href)
+            offset += 1
+            print('getting page #' + str(offset))
+            for each in resp.collection:
+                result.append(each)
+            result += self.get_all_following(offset=offset)
+        except AttributeError:
+            pass
+        for res in resp.collection:
+            result.append(res)
+        print(len(result))
+        return result
 
-    def get_most_recent_tracks(self, artist_id, age_to_disqualify=None):
-        if age_to_disqualify is None:
-            age_to_disqualify = 604800#7 days in seconds
+    # Defaults to those within the past 30 days
+    def get_most_recent_tracks(self, artist_id, age_to_disqualify=2592000):
         tracks = self.sc.get('/users/' + str(artist_id) + '/tracks')
+        with_stream = list()
+        for track in tracks:
+            try:
+                test = track.stream_url
+                with_stream.append(track)
+            except AttributeError:
+                continue
         recent = list()
         for track in tracks:
             time = self.sc_timestamp_to_datetime(track.created_at)
@@ -42,8 +65,8 @@ class soundcloud_stream_builder(object):
 
 if __name__=="__main__":
     username = raw_input("username: ")
-    start = time.clock()
+    start = time.time()
     print "building stream for " + username + "..."
     stream = soundcloud_stream_builder(username)
-    print "stream built in " + str(time.clock() - start) + " seconds"
+    print "stream built in " + str(time.time() - start) + " seconds"
 
